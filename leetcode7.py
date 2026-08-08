1bff20559521838227b22cac87077e5da59fdde4
XiaoMao <3125345282@qq.com>
2026-08-08 16:34:47 +0800

    leetcode

---
+++ leetcode7.py
@@ -0,0 +1,19 @@
+# 合并链表
+class ListNode:
+    def __init__(self,val = 0,next = None):#给引入的每个参数添加值和指针两个元素
+        self.val = val
+        self.next = next
+    def mergeTwoLists(self,list1,list2):
+        dummy = ListNode()              #创建一个链表，dummy是表头
+        current = dummy                 #定义current的初始位置，后续current从表头位置开始移动
+        while list1 and list2:
+            if list1.val < list2.val:   #list1和list2都是链表头结点，链表头结点能代表整条链表
+                current.next = list1
+                list1 = list1.next      #目的是实现指针在链表上的移动
+            else:
+                current.next = list2    #将current的指针指向list2结点
+                list2 = list2.next
+            current = current.next      #实现current的移动，保证current永远在链表的最后一位
+        current.next = list1 or list2   #while list1 and list2的对立情况，当list1或list2其中之一指向none时，剩余部分接上另一条链表
+        return dummy.next               #dummy初始位置
+        
---
+++ leetcode8.py
@@ -0,0 +1,20 @@
+# 删除倒数第n个链表
+# 依旧快慢指针
+#整体思路：fast先走n步（假设要删除倒数第n个数字），然后fast和slow一起走，直到fast走完
+# 然后把slow的next直接指向slow.next.next，把slow和slow.next.next中间那个slow.next当痘给挤了
+# 因为slow.next刚好是倒数第n个
+class ListNode:
+    def __init__(self,val = 0,next = None):
+        self.val = val
+        self.next = next
+    def removeNthFormEnd(head,n):
+        dummy = ListNode(-1,head)           #定义dummy位置，最后返回链表只要dummy之后的元素
+        slow = dummy                        #定义slow和fast的初始位置
+        fast = dummy
+        for i in range(n):                  #n=2则i=0,1，fast = fast.next执行两次
+            fast = fast.next
+        while fast.next:                    #只要链表还没被fast遍历完就继续执行后续内容
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next          #删除的数字是倒数第n个，跟正数第几无关因此dummy正引入不影响
+        return dummy.next
