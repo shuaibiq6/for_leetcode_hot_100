28a662526c16eec6ac7509e70bf8de00e63915ca
XiaoMao <3125345282@qq.com>
2026-08-09 15:12:35 +0800

    206反转链表

--- leetcode5.py
+++ leetcode5.py
@@ -2,9 +2,10 @@
 # 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
 # 输入：head = [1,2,3,4,5]
 # 输出：[5,4,3,2,1]
-def reverseList(head):
+from typing import Optional,ListNode
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
     if not head:#依旧链表的头节点就能代表整条链表，如果头结点为空则链表为空
-        return None
+        return None    #返回值类型是 Optional[ListNode]，即要么返回一个 ListNode 节点，要么返回 None。
     prev = None#定义prev和curr的初始位置
     curr = head
     while curr :#
